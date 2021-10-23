8db584096d03ca718249cd2e17f64609cc7ba4e2
Yanislav Zagorov <yanislavzagorov@yahoo.com>
2021-10-23 12:31:34 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Scene from "./Scene";
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Arealize StartCode Hackathon 2021</h1>
+        <Scene />
+      </div>
+    );
+  }
+}
+
+export default App;
