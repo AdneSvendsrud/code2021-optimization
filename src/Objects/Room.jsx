eedf8602bcc8d87444b2ad63db954001a8e2d7a7
Yanislav Zagorov <yanislavzagorov@yahoo.com>
2021-10-28 23:21:08 +0200

    Added room names

--- src/Objects/Room.jsx
+++ src/Objects/Room.jsx
@@ -1,8 +1,9 @@
 import { useRef } from "react";
 import { getRandomColor } from "../getRandomColor";
+import { Text } from "@react-three/drei";
 
 const Room = (props) => {
-  const { width, height, anchorTopLeftX, anchorTopLeftY } = props;
+  const { width, height, anchorTopLeftX, anchorTopLeftY, text } = props;
 
   const meshRef = useRef();
   const position = [width / 2 + anchorTopLeftX, 0, height / 2 + anchorTopLeftY];
@@ -11,6 +12,9 @@ const Room = (props) => {
     <mesh position={position} ref={meshRef} rotation-x={Math.PI * -0.5}>
       <planeBufferGeometry args={[width, height]} />
       <meshStandardMaterial color={getRandomColor()} />
+      <Text scale={[20, 20, 10]} anchorX="center" anchorY="center">
+        {text}
+      </Text>
     </mesh>
   );
 };
